a513ae78006c61391b433537bcaae33de1308748
queuek <lahass1213@gmail.com>
2023-03-08 23:55:38 +0900

    add init.lua

---
+++ init.lua
@@ -0,0 +1,8 @@
+
+if vim.g.vscode then
+  -- VSCode extension
+else
+  -- ordinary Neovim
+  vim.wo.number = true
+  vim.api.nvim_set_keymap('i', 'jj', '<ESC>', { noremap = true, silent = true })
+end
