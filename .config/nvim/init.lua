9957684f6746eca7c590b7600201e6093ac36df2
Ozeki Keisuke <k.ozeki@sasukefinlab.com>
2023-03-24 14:32:47 +0900

    add keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,8 @@ if vim.g.vscode then
   -- VSCode extension
 else
   -- ordinary Neovim
+  vim.g.mapleader = ' '
   vim.wo.number = true
   vim.api.nvim_set_keymap('i', 'jj', '<ESC>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', '<leader>a', 'ggVG', { noremap = true, silent = true })
 end
