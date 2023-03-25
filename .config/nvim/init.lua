32a4b8f6aba4bda09d5ab10546605ff08c8e23fc
Ozeki Keisuke <k.ozeki@sasukefinlab.com>
2023-03-25 15:34:02 +0900

    split keymap file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,10 @@
+require('keymaps')
+
 if vim.g.vscode then
   -- VSCode extension
 else
   -- ordinary Neovim
   vim.g.mapleader = ' '
-  -- vim.wo.number = true
-  vim.api.nvim_set_keymap('i', 'jj', '<ESC>', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', '<leader>a', 'ggVG', { noremap = true, silent = true })
-  vim.o.helplang = 'ja,en'
   vim.o.ignorecase = true
   vim.o.smartcase = true
   vim.o.splitright = true
---
+++ .config/nvim/lua/keymaps.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('i', 'jj', '<ESC>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>a', 'ggVG', { noremap = true, silent = true })
