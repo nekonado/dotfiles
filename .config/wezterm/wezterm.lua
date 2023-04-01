b484fad7edb8df0c2b19b5b980c558bac6fa9827
Ozeki Keisuke <k.ozeki@sasukefinlab.com>
2023-04-01 17:20:17 +0900

    add wezterm setting

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,9 @@
 return {
 	color_scheme = 'tokyonight_night',
 	window_background_opacity = 0.93,
+  font = require("wezterm").font("Firge35Nerd Console"),
+  font_size = 12.0,
+  window_frame = {
+    font_size = 10.0,
+  },
 }
