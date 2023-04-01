997f97201bffe9615dfd1c8c624512639e9aca8a
queuek <lahass1213@gmail.com>
2023-04-02 00:50:18 +0900

    fix wezterm conf

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,11 @@
+adjust_window_size_when_changing_font_size = false
+
+local wezterm = require 'wezterm'
+
 return {
 	color_scheme = 'tokyonight_night',
 	window_background_opacity = 0.93,
-  font = require("wezterm").font("Firge35Nerd Console"),
+  -- font = wezterm.font("Firge35Nerd Console"),
   font_size = 12.0,
   window_frame = {
     font_size = 10.0,
