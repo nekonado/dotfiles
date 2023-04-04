470e2902ae4e7ad8e112e6b5ff16e901b3120d5e
Ozeki Keisuke <k.ozeki@sasukefinlab.com>
2023-04-04 11:08:21 +0900

    tweak

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,42 +1,3 @@
-vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
-  pattern = { 'plugins.lua' },
-  command = 'PackerCompile',
-})
-
--- Restore cursor position when file is opened
-vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
-  pattern = { '*' },
-  callback = function()
-    vim.api.nvim_exec('silent! normal! g`"zv', false)
-  end,
-})
-
--- LSP handlers
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
-)
-
--- Reference highlight
-vim.cmd [[
-set updatetime=500
-highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-" augroup lsp_document_highlight
-"   autocmd!
-"   autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
-"   autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
-" augroup END
-]]
-
--- Colorscheme
-vim.cmd 'colorscheme tokyonight-night'
-
-vim.g.mapleader = ' '
-require 'plugins'
-require 'keymaps'
-require 'rs/index'
-
 if vim.g.vscode then
   -- VSCode extension
 else
@@ -60,4 +21,44 @@ else
   vim.wo.signcolumn = 'yes'
   vim.wo.cursorline = true
   vim.cmd 'set clipboard+=unnamedplus'
+
+  vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+    pattern = { 'plugins.lua' },
+    command = 'PackerCompile',
+  })
+
+  vim.g.mapleader = ' '
+  require 'plugins'
+  require 'keymaps'
+  require 'rs/index'
+  
+  -- Restore cursor position when file is opened
+  vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
+    pattern = { '*' },
+    callback = function()
+      vim.api.nvim_exec('silent! normal! g`"zv', false)
+    end,
+  })
+  
+  -- LSP handlers
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
+  )
+  
+  -- Reference highlight
+  vim.cmd [[
+  set updatetime=500
+  highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+  highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+  highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+  " augroup lsp_document_highlight
+  "   autocmd!
+  "   autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
+  "   autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
+  " augroup END
+  ]]
+  
+  -- Colorscheme
+  vim.cmd 'colorscheme tokyonight-night'
+  
 end
