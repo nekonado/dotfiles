8ac80f94a5ff11da05e82eb811c888353bca3f52
Ozeki Keisuke <k.ozeki@sasukefinlab.com>
2023-04-05 15:21:03 +0900

    tweak

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -47,15 +47,15 @@ else
 
   -- Reference highlight
   vim.cmd [[
-  set updatetime=500
-  highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-  highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-  highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-  " augroup lsp_document_highlight
-  "   autocmd!
-  "   autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
-  "   autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
-  " augroup END
+    set updatetime=500
+    highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+    highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+    highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+    " augroup lsp_document_highlight
+    "   autocmd!
+    "   autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
+    "   autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
+    " augroup END
   ]]
 
   -- Colorscheme
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,16 @@
 adjust_window_size_when_changing_font_size = false
 
 local wezterm = require 'wezterm'
+local config = {}
 
-return {
-	color_scheme = 'tokyonight_night',
-	window_background_opacity = 0.70,
-  text_background_opacity = 0.50,
-  -- font = wezterm.font("Firge35Nerd Console"),
+config = {
+  color_scheme = 'tokyonight_night',
+  window_background_opacity = 0.70,
+  text_background_opacity = 0.00,
   font_size = 12.0,
   window_frame = {
     font_size = 10.0,
   },
 }
+
+return config
