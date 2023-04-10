3c8cd8a61564823693c31b5ad5a2163526ef981f
queuek <lahass1213@gmail.com>
2023-04-11 02:04:41 +0900

    add gitsigns

---
+++ .config/nvim/after/plugin/nvim-treesitter.rc.lua
@@ -0,0 +1,36 @@
+local status, ts = pcall(require, "nvim-treesitter.configs")
+if (not status) then return end
+
+ts.setup {
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  ensure_installed = {
+    "tsx",
+    "toml",
+    "fish",
+    "php",
+    "json",
+    "yaml",
+    "swift",
+    "css",
+    "html",
+    "lua"
+  },
+  autotag = {
+    enable = true,
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
+  }
+}
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,15 +22,15 @@ else
     highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
     highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
     highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-    " augroup lsp_document_highlight
-    "   autocmd!
-    "   autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
-    "   autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
-    " augroup END
+    augroup lsp_document_highlight
+      autocmd!
+      autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
+      autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
+    augroup ENiD
   ]]
 
   -- Colorscheme
-  vim.cmd 'colorscheme tokyonight-moon'
+  -- vim.cmd 'colorscheme tokyonight-moon'
 
   vim.cmd 'set clipboard+=unnamedplus'
 end
