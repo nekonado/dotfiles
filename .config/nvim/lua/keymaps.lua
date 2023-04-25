10bc72526e489b221890a233a126c3e0961f32e1
queuek <lahass1213@gmail.com>
2023-04-25 22:24:44 +0900

    fix keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,5 +1,5 @@
-local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 keymap('i', 'jj', '<ESC>', opts)
 
@@ -58,15 +58,16 @@ keymap('n', '<leader>xx', '<cmd>TroubleToggle<CR>', opts)
 -- place this in one of your configuration file(s)
 local hop = require('hop')
 local directions = require('hop.hint').HintDirection
-vim.keymap.set('', 'f', function()
+
+keymap('', 'f', function()
   hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
 end, { remap = true })
-vim.keymap.set('', 'F', function()
+keymap('', 'F', function()
   hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
 end, { remap = true })
-vim.keymap.set('', 't', function()
+keymap('', 't', function()
   hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
 end, { remap = true })
-vim.keymap.set('', 'T', function()
+keymap('', 'T', function()
   hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
 end, { remap = true })
