0871a9ea21420f31dae667cb950e9d408c67fe2a
Ozeki Keisuke <k.ozeki@sasukefinlab.com>
2023-04-26 15:38:53 +0900

    fix sandboxfile

--- .config/nvim/sandbox/add.lua
+++ .config/nvim/sandbox/add.lua
@@ -1,3 +1,5 @@
+-- https://zenn.dev/slin/articles/2020-10-19-neovim-lua1#neovim%E3%81%AE%E6%A8%99%E6%BA%96%E3%83%A9%E3%82%A4%E3%83%96%E3%83%A9%E3%83%AA
+
 -- local a = 100
 -- local b = 200
 -- print(a + b)
