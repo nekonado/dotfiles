3d877301e3be8643171fcd86b18bb2edf1ab934a
Ozeki Keisuke <k.ozeki@sasukefinlab.com>
2023-04-26 15:37:17 +0900

    add lua sandbox

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -71,3 +71,6 @@ end, { remap = true })
 keymap('', 'T', function()
   hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
 end, { remap = true })
+
+-- lua program execution
+keymap('n', '<F5>', '<cmd>luafile %<CR>', opt)
---
+++ .config/nvim/sandbox/add.lua
@@ -0,0 +1,18 @@
+-- local a = 100
+-- local b = 200
+-- print(a + b)
+
+-- local array = {
+--   100, 200, 300
+-- }
+
+-- print(array[1] .. (array[2] + array[3]))
+-- print(array)
+-- print(pairs(array))
+-- print(ipairs(array))
+
+-- local i = 0
+-- while i < 100 do
+--   print(i)
+--   i = i + 1
+-- end
