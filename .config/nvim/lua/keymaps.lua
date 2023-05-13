ba6bd87eecf74eb5d5430a59afa6057281c17e51
queuek <lahass1213@gmail.com>
2023-05-13 10:34:47 +0900

    fix terminal mode keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,51 +1,54 @@
 local keymap = vim.keymap.set
-local opts = { noremap = true, silent = true }
+local op_nr = { noremap = true, silent = true }
+local op_r = { remap = true, silent = true }
 
-keymap('i', 'jj', '<ESC>', opts)
-keymap('n', ';', ':', opts)
+keymap('i', 'jj', '<ESC>', op_nr)
+keymap('t', 'jj', '<ESC>', op_r)
 
-keymap('n', 'H', '0', opts)
-keymap('n', 'L', '$', opts)
+keymap('n', ';', ':', op_nr)
 
-keymap('n', 'Y', 'y$', opts)
-keymap('n', 'P', ']P', opts)
-keymap('n', 'U', '<C-r>', opts)
-keymap('n', '+', '<C-a>', opts)
-keymap('n', '-', '<C-x>', opts)
+keymap('n', 'H', '0', op_nr)
+keymap('n', 'L', '$', op_nr)
+
+keymap('n', 'Y', 'y$', op_nr)
+keymap('n', 'P', ']P', op_nr)
+keymap('n', 'U', '<C-r>', op_nr)
+keymap('n', '+', '<C-a>', op_nr)
+keymap('n', '-', '<C-x>', op_nr)
 
 -- Better window navigation
-keymap('n', '<C-h>', '<C-w>h', opts)
-keymap('n', '<C-j>', '<C-w>j', opts)
-keymap('n', '<C-k>', '<C-w>k', opts)
-keymap('n', '<C-l>', '<C-w>l', opts)
+keymap('n', '<C-h>', '<C-w>h', op_nr)
+keymap('n', '<C-j>', '<C-w>j', op_nr)
+keymap('n', '<C-k>', '<C-w>k', op_nr)
+keymap('n', '<C-l>', '<C-w>l', op_nr)
 
 -- Emacs style text edit
-keymap('i', '<C-a>', '<C-o>0', opts)
-keymap('i', '<C-e>', '<C-o>$', opts)
-keymap('i', '<C-h>', '<BS>', opts)
-keymap('i', '<C-d>', '<Del>', opts)
-keymap('i', '<C-k>', '<C-o>D', opts)
+keymap('i', '<C-a>', '<C-o>0', op_nr)
+keymap('i', '<C-e>', '<C-o>$', op_nr)
+keymap('i', '<C-h>', '<BS>', op_nr)
+keymap('i', '<C-d>', '<Del>', op_nr)
+keymap('i', '<C-k>', '<C-o>D', op_nr)
 
 -- Better switch visual mode
-keymap('n', 'v', '<C-v>', opts)
-keymap('n', '<C-v>', 'v', opts)
+keymap('n', 'v', '<C-v>', op_nr)
+keymap('n', '<C-v>', 'v', op_nr)
 
 -- terminal
-keymap('t', '<Esc>', '<C-\\><C-n>', opts)
-keymap('t', '<C-c>', '<C-\\><C-n>', opts)
+keymap('t', '<Esc>', '<C-\\><C-n>', op_nr)
+keymap('t', '<C-c>', '<C-\\><C-n>', op_nr)
 
 -- leader key
-keymap('n', '<leader>a', 'ggVG', opts)
-keymap('n', '<leader>o', 'o<ESC>', opts)
-keymap('n', '<leader>O', 'O<ESC>', opts)
-keymap('n', '<leader><Esc>', '<cmd>nohlsearch<CR>', opts)
-keymap('n', '<leader>w', '<cmd>Chowcho<CR>', opts)
-keymap('n', '<leader>e', '<cmd>Neotree<CR>', opts)
+keymap('n', '<leader>a', 'ggVG', op_nr)
+keymap('n', '<leader>o', 'o<ESC>', op_nr)
+keymap('n', '<leader>O', 'O<ESC>', op_nr)
+keymap('n', '<leader><Esc>', '<cmd>nohlsearch<CR>', op_nr)
+keymap('n', '<leader>w', '<cmd>Chowcho<CR>', op_nr)
+keymap('n', '<leader>e', '<cmd>Neotree<CR>', op_nr)
 
 -- HopNvim
-keymap('n', 'hw', '<cmd>HopWord<CR>', opts)
-keymap('n', 'hp', '<cmd>HopPattern<CR>', opts)
-keymap('n', 'ht', '<cmd>HopChar2<CR>', opts)
+keymap('n', 'hw', '<cmd>HopWord<CR>', op_nr)
+keymap('n', 'hp', '<cmd>HopPattern<CR>', op_nr)
+keymap('n', 'ht', '<cmd>HopChar2<CR>', op_nr)
 
 -- Telescope
 keymap('n', '<leader>ff', '<cmd>Telescope find_files<CR>', {})
@@ -58,14 +61,14 @@ keymap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', {})
 keymap("n", "<leader>fl", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", {})
 
 -- ToggleTerm
-keymap('n', '<leader>th', '<cmd>ToggleTerm direction=horizontal<CR>', opts)
-keymap('n', '<leader>2th', '<cmd>2 ToggleTerm direction=horizontal<CR>', opts)
-keymap('n', '<leader>3th', '<cmd>3 ToggleTerm direction=horizontal<CR>', opts)
-keymap('n', '<leader>tt', '<cmd>ToggleTermToggleAll<CR>', opts)
-keymap('n', '<leader>tf', '<cmd>ToggleTerm direction=float<CR>', opts)
+keymap('n', '<leader>th', '<cmd>ToggleTerm direction=horizontal<CR>', op_nr)
+keymap('n', '<leader>2th', '<cmd>2 ToggleTerm direction=horizontal<CR>', op_nr)
+keymap('n', '<leader>3th', '<cmd>3 ToggleTerm direction=horizontal<CR>', op_nr)
+keymap('n', '<leader>tt', '<cmd>ToggleTermToggleAll<CR>', op_nr)
+keymap('n', '<leader>tf', '<cmd>ToggleTerm direction=float<CR>', op_nr)
 
 -- Trouble
-keymap('n', '<leader>xx', '<cmd>TroubleToggle<CR>', opts)
+keymap('n', '<leader>xx', '<cmd>TroubleToggle<CR>', op_nr)
 
 -- place this in one of your configuration file(s)
 local hop = require('hop')
@@ -85,4 +88,4 @@ keymap('', 'T', function()
 end, { remap = true })
 
 -- lua program execution
-keymap('n', '<F5>', '<cmd>luafile %<CR>', opts)
+keymap('n', '<F5>', '<cmd>luafile %<CR>', op_nr)
